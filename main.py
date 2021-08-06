e6e197244c0e8e1c809ffaa8d4a243e2b536e444
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-08-06 23:13:16 +0700

    Add password generator

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 from tkinter import *
 from tkinter import messagebox
+from random import shuffle, choice, randint
+
+
 FONT_NAME = "Arial"
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_pass():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = randint(8, 10)
+    nr_symbols = randint(2, 4)
+    nr_numbers = randint(2, 4)
+
+    password_list = []
+
+    password_list += [choice(letters) for _ in range(randint(8, 10))]
+    password_list += [choice(symbols) for _ in range(randint(2, 4))]
+    password_list += [choice(numbers) for _ in range(randint(2, 4))]
+
+
+    shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+      password += str(char)
+    pass_input.delete(0, END)
+    pass_input.insert(0, password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_pass():
@@ -61,7 +87,7 @@ pass_input.grid(row=3, column=1)
 add_button = Button(text="Add", bg="lavender", bd=0, width=36, font=(FONT_NAME, 14, "bold"), command=add_pass)
 add_button.grid(row=4, column=1, columnspan=2)
 
-gen_button = Button(text="Generate Password", bg="lavender", bd=0, width=15, font=(FONT_NAME, 12, "normal"))
+gen_button = Button(text="Generate Password", bg="lavender", bd=0, width=15, font=(FONT_NAME, 12, "normal"), command=generate_pass)
 gen_button.grid(row=3, column=2)
 
 window.mainloop()
